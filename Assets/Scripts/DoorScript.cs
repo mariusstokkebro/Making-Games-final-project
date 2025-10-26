dd8519aef7c6bd52d85e1f701d94d9c0a503d3dd
Marius Stokkebro <masto@itu.dk>
2025-10-26 19:21:54 +0100

    camera moves when going to another room

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -1,22 +1,53 @@
-using System.Runtime.CompilerServices;
+
+
 using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
 
+    LayerMask layerMask;
+
+    void Start()
+    {
+        layerMask = LayerMask.GetMask("Room");
+
+    }
     void OnTriggerStay(Collider other)
     {
+
         if (other.CompareTag("Player"))
         {
-
-            Debug.Log("Door opened");
+            //Debug.Log("Door opened");
+            Transform room = GetClosestRoom();
+            MoveCameraToRoom(room);
 
         }
     }
 
-    // Update is called once per frame
-    void GetClosestRoom()
+    Transform GetClosestRoom()
     {
-        Vector3 back = transform.TransformDirection(Vector3.backwards);
+        RaycastHit hit;
+        Vector3 back = transform.TransformDirection(Vector3.left);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * 1000, Color.white);
+        if (Physics.Raycast(transform.position, back, out hit, 20, layerMask))
+        {
+            return hit.transform;
+        }
+        return null;
+
+    }
+    void MoveCameraToRoom(Transform room)
+    {
+        Transform cameraPoint = room.Find("cameraPoint");
+        if (cameraPoint != null)
+        {
+            Camera.main.transform.position = cameraPoint.position;
+            Camera.main.transform.rotation = cameraPoint.rotation;
+        }
+        else
+        {
+            Debug.LogWarning($"No cameraPoint found in {room.name}");
+        }
     }
 }
+
